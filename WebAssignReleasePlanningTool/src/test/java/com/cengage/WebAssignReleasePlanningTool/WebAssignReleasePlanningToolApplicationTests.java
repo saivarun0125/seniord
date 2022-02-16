609c94c4c1c4ad65416de1e9d54fbfffa73d7cc9
adschne2 <adschne2@ncsu.edu>
2022-02-16 10:56:46 -0500

    initial commit

---
+++ WebAssignReleasePlanningTool/src/test/java/com/cengage/WebAssignReleasePlanningTool/WebAssignReleasePlanningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengage.WebAssignReleasePlanningTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAssignReleasePlanningToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
