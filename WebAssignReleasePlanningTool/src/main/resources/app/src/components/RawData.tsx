9c6286a9d87865657ce6232eb1b79f727abcf96f
snaru <snaru@ncsu.edu>
2022-03-22 09:48:30 -0400

    Implemented export functionality for the button

--- WebAssignReleasePlanningTool/src/main/resources/app/src/components/RawData.tsx
+++ WebAssignReleasePlanningTool/src/main/resources/app/src/components/RawData.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
-import {  Button, Datagrid, DropdownDropDirection } from 'react-magma-dom';
+import {  Button, ButtonVariant, Datagrid, DropdownDropDirection } from 'react-magma-dom';
+import { ExportToCsv } from 'export-to-csv';
 
 export default function RawData(props:{columns: any[], rows: any[]}) {
+
+    const options = { 
+        fieldSeparator: ',',
+        quoteStrings: '"',
+        decimalSeparator: '.',
+        showLabels: true, 
+        showTitle: true,
+        title: 'raw_data',
+        useTextFile: false,
+        useBom: true,
+        useKeysAsHeaders: true,
+    };
+
+    function export_data() {
+        const csvExporter = new ExportToCsv(options);
+        csvExporter.generateCsv(props.rows);
+    }
 
     return (<>
             <Datagrid
@@ -11,7 +29,7 @@ export default function RawData(props:{columns: any[], rows: any[]}) {
                     pagination: { dropdownDropDirection: DropdownDropDirection.down },
                 }}
             />
-            <Button> Export </Button>
+            <Button onClick={export_data} variant={ButtonVariant.outline}> Export </Button>
             </>
     );
 }
