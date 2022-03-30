d271a11ff946e3b0c8479187994d4b0cbad39941
adschne2 <adschne2@ncsu.edu>
2022-03-30 19:16:41 -0400

    created test cases for due date report that interacts with test database

---
+++ WebAssignReleasePlanningTool/src/test/java/com/cengage/WebAssignReleasePlanningTool/controllers/DueDateReportControllerTest.java
@@ -0,0 +1,85 @@
+package com.cengage.WebAssignReleasePlanningTool.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.hibernate.mapping.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.cengage.WebAssignReleasePlanningTool.WebAssignReleasePlanningToolApplication;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = WebAssignReleasePlanningToolApplication.class)
+@WebAppConfiguration
+@ActiveProfiles("test")
+class DueDateReportControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+	@Test
+	void test() {
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		ResultActions ra = null;
+		try {
+			ra = mockMvc.perform(MockMvcRequestBuilders.get("/duedatereport")
+									.param("startDate", "2022-03-12T00:00:00.000Z")
+									.param("endDate", "2022-03-13T23:59:59.000Z")
+									.param("duration", "90"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		assertNotNull(ra);
+		
+		try {
+			//System.err.println(ra.andReturn().getResponse().getContentAsString());
+			ra.andExpect(jsonPath("items").isArray());
+			
+			int idx = 0;
+			while(true)
+			{
+				try {
+					ra.andExpect(jsonPath("items[" + idx + "].id").value(idx++));
+				} 
+				catch (AssertionError e)
+				{
+					if(e.getMessage().contains("No value at JSON path"))
+					{
+						break;	
+					}
+					else
+					{
+						fail();
+					}
+				}
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+}
