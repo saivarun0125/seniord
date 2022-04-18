585349718fef3f45290394d0c21cd6c7032b05fb
adschne2 <adschne2@ncsu.edu>
2022-04-18 15:51:11 -0400

    repository endpoints now reflect the design in the wiki page

--- WebAssignReleasePlanningTool/src/main/java/com/cengage/WebAssignReleasePlanningTool/controllers/RepositoryController.java
+++ WebAssignReleasePlanningTool/src/main/java/com/cengage/WebAssignReleasePlanningTool/controllers/RepositoryController.java
@@ -1,6 +1,9 @@
 package com.cengage.WebAssignReleasePlanningTool.controllers;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,9 +23,21 @@ public class RepositoryController {
     RepositoryRepository repositoryRepository;
 
     @GetMapping ( "/repositories" )
-    public List<Repository> getRepositories () {
-
-        return repositoryRepository.findAll();
+    public List<Map<String, Object>> getRepositories ()
+    {
+    	List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        List<Repository> repo = repositoryRepository.findAll();
+    
+        //add name and id for each repo
+        for(Repository r : repo)
+        {
+        	Map<String, Object> map = new LinkedHashMap<String, Object>();
+        	map.put("id", r.getId());
+        	map.put("name", r.getName());
+        	list.add(map);
+        }
+        
+        return list;
     }
 
 }
