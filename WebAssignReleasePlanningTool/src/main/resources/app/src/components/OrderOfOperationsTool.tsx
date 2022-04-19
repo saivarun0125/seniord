f2b37de9d1ad0f29bace93295a8ba444071dcbe8
snaru <snaru@ncsu.edu>
2022-04-19 09:19:26 -0400

    Added endpoint to ooot ts file

--- WebAssignReleasePlanningTool/src/main/resources/app/src/components/OrderOfOperationsTool.tsx
+++ WebAssignReleasePlanningTool/src/main/resources/app/src/components/OrderOfOperationsTool.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { useState } from "react";
+import axios, { AxiosError } from "axios";
 import { 
     Button, 
     Breadcrumb, 
@@ -9,7 +11,26 @@ import {
     FlexBehavior
 } from 'react-magma-dom';
 
+export interface Release {
+    name: String,
+    id: String
+}
+
 export default function OrderOfOperationsTool() {
+
+    const emptyRelease: Release = {
+        name: '',
+        id: ''
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [options, setOptions] = useState([] as Release[]);
+    const [selectedRelease, setSelectedRelease] = useState(emptyRelease);
+
+    const handleSubmit = {
+        //return <Release release={selectedRelease}/>;
+    };
+
     const dummyOptions = [
         {
           label: "release #1",
@@ -24,7 +45,29 @@ export default function OrderOfOperationsTool() {
           value: "45"
         }
       ];
+    
+    function onSelectedItemChange(changes: any) {
+        setSelectedRelease(changes.selectedItem);
+    }
+    function optionsToString(item: Release) {
+        return item ? `${item.name}` : '';
+    }
 
+    function getReleases() {
+        var url = "http://localhost:8080/release";
+        console.log(url);
+
+        axios.get(url)
+            .then((response) => response.data)
+            .then((data) => {
+                console.log(data);
+                setOptions(data);
+            })
+            .catch((error: AxiosError) => {
+                console.log(error.message);
+            });
+    }
+    getReleases();
     return(<>
         <Breadcrumb style={{padding:"20px"}}>
             <BreadcrumbItem to="/home">Home</BreadcrumbItem>
@@ -45,7 +88,12 @@ export default function OrderOfOperationsTool() {
             <Flex behavior={FlexBehavior.item} xs={11}>
             </Flex>
             <Flex behavior={FlexBehavior.item} xs={2}>
-                <Select id="existingRelease" labelText="Select Existing Release:" placeholder="Select..." items={dummyOptions} />
+                <Select id="existingRelease" labelText="Select Existing Release:" placeholder="Select..." items={options} itemToString={optionsToString} onSelectedItemChange={onSelectedItemChange}/>
+            </Flex>
+            <Flex behavior={FlexBehavior.item} xs={10}>
+            </Flex>
+            <Flex behavior={FlexBehavior.item} xs={2}>
+                    <Button /*onClick={handleSubmit}*/> Submit </Button>
             </Flex>
         </Flex>
     </>);
